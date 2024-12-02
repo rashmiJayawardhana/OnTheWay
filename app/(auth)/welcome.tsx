475e520e318da2aa3a3a5f2adcce8b5791a9a534
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2024-12-03 01:24:58 +0530

    update

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,56 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-center text-lg font-JakartaSemiBold mx-10 mt-3 text-[#858585]">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profie = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profie</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profie;
