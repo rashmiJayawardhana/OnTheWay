4236bf2d7cd272f3b81d0705a272bfc18eddce64
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2024-12-13 16:33:00 +0530

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,12 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
-const Index = () => {
-  const { isLoaded, isSignedIn } = useAuth();
+const Page = () => {
+  const { isSignedIn } = useAuth();
 
-  // Ensure authentication state is fully loaded before making a decision
-  if (!isLoaded) return null; // Show nothing or a loading spinner while auth state is loading
-
-  // Redirect based on authentication status
-  if (isSignedIn) {
-    return <Redirect href="/(root)/(tabs)/home" />;
-  }
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
 
   return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Index;
+export default Page;
